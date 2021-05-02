63b70f58e07eb5ec643c1341d9b860c7ee2983c9
David Griswold <49095734+DavidGriswoldTeacher@users.noreply.github.com>
2021-05-02 00:04:31 -0300

    first commit

---
+++ src/Sketch.java
@@ -0,0 +1,42 @@
+import processing.core.PApplet;
+
+/**
+ * This class defines the Processing Sketch It extends PApplet, which is a
+ * special processing class to represent the sketches
+ * 
+ * https://processing.org/reference/ is a helpful site to help you better
+ * understad the built-in processing functions
+ */
+public class Sketch extends PApplet {
+
+    /**
+     * This function runs at the very beginning and defines the settings of the
+     * sketch. Only certain methods are allowed in here!
+     */
+    public void settings() {
+        size(500, 500);
+    }
+
+    /**
+     * This function runs after settings, and initializes your code. You can put
+     * whatever you want in here, but it only runs once at the beginning!
+     */
+    public void setup() {
+        // paint the background gray
+        background(100);
+    }
+
+    /**
+     * This function runs over and over and over, approximately 60 times per second!
+     */
+    public void draw() {
+
+        // draw a ball where the mouse is
+        circle(mouseX, mouseY, 50);
+    }
+
+    /** All processing sketches have to use this main method */
+    public static void main(String[] args) {
+        PApplet.main("Sketch");
+    }
+}
